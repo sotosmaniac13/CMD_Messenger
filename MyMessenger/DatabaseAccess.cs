44ef1483ef0ec2298129cafc84e45b016b051707
Sotiris Maniatakos <sotosmaniac@yahoo.com>
2018-12-22 17:03:37 +0200

    Establishing connection with the database and inserting values for UserDetails, LoginCredentials and UserEmail tables

--- MyMessenger/DatabaseAccess.cs
+++ MyMessenger/DatabaseAccess.cs
@@ -10,50 +10,87 @@ namespace MyMessenger
 {
     public class DatabaseAccess
     {
-        static string connectionString = "Server=DESKTOP-NO1CDE8\\SQLEXPRESS; Database=MyMessengerDB; User Id=admin; Password=admin";
+        static string connectionString = "Server=Sotiros-MiniPC\\SQLEXPRESS; Database=MyMessengerDB; User Id=admin; Password=admin";
+        SqlConnection dbConnection = new SqlConnection(connectionString);
 
         public void InsertNewUser()
         {
-            SqlConnection dbConnection = new SqlConnection(connectionString);
-
             dbConnection.Open();
 
             //INSERT NEW USER IN THE DATABASE
             Console.WriteLine("================\nNEW USER ACCOUNT\n================\n");
-            Console.WriteLine("Enter your Username:");
+
+            
+            //Asking new user for his details in order to create an account
+            Console.Write("Enter your Username: ");
             string newUsername = Console.ReadLine();
-            Console.WriteLine("Enter your Password:");
+            Console.Write("Enter your Password: ");
             string newPassword = Console.ReadLine();
-
-            Console.WriteLine("Enter your Firstname:");
+            Console.Write("Enter your Firstname: ");
             string newFirstName = Console.ReadLine();
-            Console.WriteLine("Enter your Lastname:");
+            Console.Write("Enter your Lastname: ");
             string newLastName = Console.ReadLine();
-            Console.WriteLine("Enter your Age:");
+            Console.Write("Enter your Age: ");
             string newAge = Console.ReadLine();
-            Console.WriteLine("Enter your Email Address:");
+            Console.Write("Enter your Email Address: ");
             string newEmail = Console.ReadLine();
-
+            //Hashing for securely storing the password in the database
             string hashedPassword = PasswordHashing.sha256_hash(newPassword);
 
-            var insertNewUserLogin = new SqlCommand("INSERT INTO LoginCredentials VALUES(@U_Username, @U_Password)", dbConnection);
-            insertNewUserLogin.Parameters.AddWithValue("@U_Username", newUsername);
-            insertNewUserLogin.Parameters.AddWithValue("@U_Password", hashedPassword);
 
-            var affectedrows = insertNewUserLogin.ExecuteNonQuery();
-            Console.WriteLine(affectedrows);
+            //Reading all User Ids from every table in the database and produces new Ids for the new user in each table
+            var newUserId = NewIdCreation("UserId", "UserDetails");
+            var newLoginId = NewIdCreation("LoginId", "LoginCredentials");
+            var newEmailId = NewIdCreation("EmailId", "UserEmail");
 
-            var insertNewUserDetails = new SqlCommand("INSERT INTO UserDetails VALUES(@FirstName, @LastName, @Age)", dbConnection);
+
+            //Saving User Details in the database
+            var insertNewUserDetails = new SqlCommand("INSERT INTO UserDetails VALUES(@UserId, @FirstName, @LastName, @Age, @JoinedAppOn, @UserRole)", dbConnection);
+            insertNewUserDetails.Parameters.AddWithValue("@UserId", newUserId);
             insertNewUserDetails.Parameters.AddWithValue("@FirstName", newFirstName);
             insertNewUserDetails.Parameters.AddWithValue("@LastName", newLastName);
             insertNewUserDetails.Parameters.AddWithValue("@Age", newAge);
+            insertNewUserDetails.Parameters.AddWithValue("@JoinedAppOn", DateTime.Now.ToString("d/M/yyyy"));
+            insertNewUserDetails.Parameters.AddWithValue("@UserRole", "User1");
+            var affectedrows = insertNewUserDetails.ExecuteNonQuery();
 
-            var affectedrows2 = insertNewUserDetails.ExecuteNonQuery();
-            Console.WriteLine(affectedrows2);
+            //Saving Login Credentials in the database
+            var insertNewUserLogin = new SqlCommand("INSERT INTO LoginCredentials VALUES(@LoginId, @UserId, @U_Username, @U_Password)", dbConnection);
+            insertNewUserLogin.Parameters.AddWithValue("@LoginId", newLoginId);
+            insertNewUserLogin.Parameters.AddWithValue("@UserId", newUserId);
+            insertNewUserLogin.Parameters.AddWithValue("@U_Username", newUsername);
+            insertNewUserLogin.Parameters.AddWithValue("@U_Password", hashedPassword);
+            var affectedrows2 = insertNewUserLogin.ExecuteNonQuery();
+
+            //Saving User Email in the database
+            var insertNewUserEmail = new SqlCommand("INSERT INTO UserEmail VALUES(@EmailId, @UserId, @EmailAddress)", dbConnection);
+            insertNewUserEmail.Parameters.AddWithValue("@EmailId", newEmailId);
+            insertNewUserEmail.Parameters.AddWithValue("@UserId", newUserId);
+            insertNewUserEmail.Parameters.AddWithValue("@EmailAddress", newEmail);
+            var affectedrows3 = insertNewUserEmail.ExecuteNonQuery();
 
             dbConnection.Close();
         }
 
+        
+        //A method for checking existing Ids in a table and producing a new unique id
+        private int NewIdCreation(string columnName, string tableName)
+        {
+            var selectQuery = ("SELECT " + columnName + " FROM " + tableName);
+            var checkForExistingIds = new SqlCommand(selectQuery, dbConnection);
+            var idsList = new List<int>();
+            var reader = checkForExistingIds.ExecuteReader();
 
+            int i = 0;
+            while (reader.Read())
+                idsList.Add(reader.GetInt32(i));
+            reader.Close();
+
+            var newId = 1;
+            while (idsList.Contains(newId))
+                newId++;
+
+            return newId;
+        }
     }
 }
